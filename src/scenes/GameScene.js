4426c96550e18b57446d88dd429da0543fb03d35
Luka Harambasic <luka@harambasic.de>
2023-05-04 23:12:39 -0700

    need to explicitly set type to WEBGL

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -6,8 +6,17 @@ export class GameScene extends Scene {
   }
 
   create() {
+    this.leftBarrier = this.physics.add.sprite(32, centerY, 'paddle').setOrigin(0.5)
+    this.rightBarrier = this.physics.add.sprite(32, centerY, 'paddle').setOrigin(0.5)
+    // paddle.setCollideWorldBounds(true)
+    // paddle.setBounce(0.5)
+    // paddle.setImmovable()
+    // paddle.setMaxVelocity(0, 600)
+    // paddle.setDragY(200)
+    // paddle.setDepth(1) // ensures that paddle z-depth remains above shadow paddles
+    // paddle.destroyed = false // custom property to track paddle life
+    // paddle.setBlendMode('SCREEN') // set a WebGL blend mode
   }
 
-  update() {
-  }
+  update() {}
 }
